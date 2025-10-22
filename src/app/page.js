5d03db1c5aeac66ce5d3145de5bf4b0df178359f
Santiago <santiagobarinas230@gmail.com>
2025-10-22 12:10:46 -0500

    Actualizacion Del CRUD

--- src/app/page.js
+++ src/app/page.js
@@ -2,40 +2,21 @@
 import styles from "./page.module.css";
 import { useRouter } from "next/navigation";
 
+
+
 export default function Home() {
-  const router = useRouter();
 
-  const goRegister = () =>{
-    router.push("/register/")
-  };
-
-  const goinicio = () => {
-    router.push ("/inicio/")
-  }
-  
   return (
-    <div className="container">
-      <div className="loginContainer">
-        <h2>INICIO DE SESION</h2>
-
-        <div className="Usuario">
-          <input type="text" required/>
-          <label htmlFor="Nombre">Usuario</label>
+    <div className="Container">
+      <div className="Login">
+        <h1>Inicio</h1>
+        <div className="knot-right">
+          <img src="/Inicio.png" alt="Inicio" />
         </div>
-
-        <div className="Usuario">
-          <input type="password" required/>
-          <label htmlFor="Nombre">Contraseña</label>
-        </div>
-        
-        <button className="button"><a onClick={goinicio}>Iniciar</a></button>
-
-        <p className="register-text">
-          ¿No tienes cuenta? <a onClick={goRegister}>Regístrate</a>
-        </p>
-      </div>
+      </div> 
     </div>
-
   );
-  
+
+
+
 }
